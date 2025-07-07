4da2804de142906f52a492d092f80f7728309210
Michael <100490812+DebSol11@users.noreply.github.com>
2025-07-07 16:14:15 +0200

    Art in progress!

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 const body = document.querySelector("body");
 const table = document.querySelector("table");
+const submitBtn = document.querySelector("#submitBtn");
 
 const myLibrary = [
   {
     title: "The Bible",
-    author: "GOD",
+    author: "?",
     pages: "a lot",
-    read: "not read yet",
+    read: "false",
+    notReadYet: "true",
   },
   {
     title: "The Koran",
-    author: "GOD",
+    author: "?",
     pages: "a lot",
-    read: "not read yet",
+    read: "false",
+    notReadYet: "true",
   },
   {
     title: "The Tanach",
-    author: "GOD",
+    author: "?",
     pages: "a lot",
-    read: "not read yet",
+    read: "false",
+    notReadYet: "true",
   },
   {
     title: "Pastafari",
     author: "Fliegendes Spaghettimonster",
     pages: "a lot",
-    read: "not read yet",
+    read: "false",
+    notReadYet: "true",
   },
 ];
 
@@ -35,7 +40,12 @@ function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  // if (read == false || read == true) {
   this.read = read;
+  // } else {
+  //   throw Error("You must chose here between true and false");
+  // }
+  this.notReadYet = !read;
   this.info = function () {
     return `${title} by ${author}, ${pages} pages, ${read}`;
   };
@@ -50,8 +60,8 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 console.log(myLibrary);
-console.log(addBookToLibrary("lol", "lolus", "77", "not read yet"));
-console.log(addBookToLibrary("test", "test2", "122", "read"));
+console.log(addBookToLibrary("lol", "lolus", "77", "true"));
+console.log(addBookToLibrary("test", "test2", "122", "false"));
 
 function displayBooks() {
   for (let i = 0; i < myLibrary.length; i++) {
@@ -59,23 +69,103 @@ function displayBooks() {
     content.innerHTML = `<td>${myLibrary[i].title}</td>
             <td>${myLibrary[i].author}</td>
             <td>${myLibrary[i].pages}</td>
-            <td>${myLibrary[i].read}</td>`;
+            <td>${myLibrary[i].read}</td>
+            <td>${myLibrary[i].notReadYet}</td>`;
     table.appendChild(content);
   }
 }
 
 function addButton() {
-    const newBookButton = document.createElement("button");
-    newBookButton.textContent = "New Book";
-    body.appendChild(newBookButton);
+  const newBookButton = document.createElement("button");
+  newBookButton.textContent = "New Book";
+  newBookButton.classList.add("newBookButton");
+  body.appendChild(newBookButton);
 }
 
 displayBooks();
 addButton();
 
-function displayForm() {
-  const form = document.createElement("form");
-  // form.innerHTML = 
+const newBookButton = document.querySelector(".newBookButton");
 
+// function displayForm() {
+//   newBookButton.addEventListener("click", () => {
+//     const form = document.createElement("form");
+//     // form.innerHTML = `<form action="example.com/path" method="post">
+//     //   <label for="title">Title:</label>
+//     //   <input type="text" id="title" name="title" placeholder="The Bible" />
+//     //   <label for="author">Author:</label>
+//     //   <input type="text" id="author" name="author" placeholder="Who knows?" />
+//     //   <label for="pages">Pages:</label>
+//     //   <input type="number" id="pages" name="pages" placeholder="50" />
+//     //   <label for="read">read?</label>
+//     //   <input type="radio" id="read" name="read_status" value="read" />
+//     //   <label for="not read yet">not read yet?</label>
+//     //   <input
+//     //     type="radio"
+//     //     id="not read yet"
+//     //     name="read_status"
+//     //     value="not read yet"
+//     //   />
+//     //   <button type="submit" onclick="storeInput()" id="submitBtn">Submit</button>
+//     // </form>`;
+//     body.appendChild(form);
+//   });
+// }
 
+// displayForm();
+
+const form = document.querySelector("form");
+
+submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+
+function storeInput() {
+  // Initialize an empty object
+  let inputObject = {};
+
+  // Get the input elements
+  const title = document.getElementById("title");
+  const author = document.getElementById("author");
+  const pages = document.getElementById("pages");
+  const readStatus = document.querySelector('input[name="rate_status"]:checked').value;
+  // const read = document.getElementById("read");
+  // const notReadYet = document.getElementById("not-read-yet");
+
+  // Get the values of the input elements
+  const value1 = title.value;
+  const value2 = author.value;
+  const value3 = pages.value;
+  const value4 = readStatus.value;
+  // const value5 = notReadYet.value;
+
+  // Add the values to the object
+  inputObject = {
+    title: value1,
+    author: value2,
+    pages: value3,
+    readStatus: value4,
+    // read: value4,
+    // notReadYet: value5,
+  };
+
+  // Print the array to the console
+  console.log(inputObject);
 }
+
+// function createNewBook() {
+//   form.addEventListener("submit", (event) => {
+//     event.preventDefault();
+
+//     let bookFormInputs = {
+//       name: form.elements.title.value,
+//       author: form.elements.author.value,
+//       pages: form.elements.pages.value,
+//       read: form.elements.read.value,
+//     }
+//     console.log(bookFormInputs);
+//     return bookFormInputs;
+//   });
+// }
+
+// createNewBook();
